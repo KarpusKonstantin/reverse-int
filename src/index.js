f03e8735d47b3977a1277c3f1c6c4b7aadd23ebb
KarpusKonstantin <60568968+KarpusKonstantin@users.noreply.github.com>
2020-03-06 13:06:33 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function reverse (n) {
-  
+  let m = [];
+  let s;
+  let res = 0;
+
+  if (n < 0) { a = false; }
+
+  m = String(n).split('');
+  m.reverse();
+  s = m.join('');
+
+  res = parseInt(s);
+
+//  console.log('IN = ', n, 'RESULT = ', res);
+  return res;
+
 }
